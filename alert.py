a9cb06a932e9dae6695a673d2856d3cde013f3e9
Rafael Ferreira <mail@rafaelferreira.pt>
2014-11-02 21:52:02 +0000

    multi account, ping apache server, send alert emails, send report via email

---
+++ alert.py
@@ -0,0 +1,38 @@
+__author__ = 'gipmon'
+from email.mime.text import MIMEText
+import smtplib
+import datetime
+
+username = "EMAIL"
+password = "PASSWORD"
+smtp_url = "SMTP_URL"
+smtp_port = 587 # PORT NUMBER
+
+def send_alert_droplet_down(femail, temail, droplet):
+    send_email(femail, temail, "[DIGITAL OCEAN BACKUPS] Port down", "The droplet: "+droplet.name+" is down!")
+
+def send_report(femail, temail, report):
+    send_email(femail, temail, "[DIGITAL OCEAN BACKUPS][REPORT]["+str(datetime.datetime.now().day)+"/"+\
+               str(datetime.datetime.now().month)+"/"+str(datetime.datetime.now().year)+"]", report)
+
+def send_email(femail, temail, subject, text):
+    global username, passoword, smtp_url, smtp_port
+
+    fromaddr = femail
+    toaddr = temail
+
+    msg = MIMEText(text)
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    msg['Subject'] = subject
+
+    server = smtplib.SMTP(smtp_url, smtp_port)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    server.login(username, password)
+
+    text = msg.as_string()
+
+    server.sendmail(fromaddr, toaddr, text)
+    server.quit()
--- snapshots.py
+++ snapshots.py
@@ -2,42 +2,52 @@ __author__ = 'gipmon'
 import sys
 import pyocean
 import datetime
+import alert
+import droplet_status
 
-ACCESS_TOKEN = "YOUR_ACCESS_TOKEN"
-digitalocean = pyocean.DigitalOcean(ACCESS_TOKEN)
-now = datetime.datetime.now()
-max_snaps = 5
+ACCESS_TOKEN1 = "YOUR_ACCESS_TOKEN"
+ACCESS_TOKEN2 = "YOUR_ACCESS_TOKEN"
+accountsList = [pyocean.DigitalOcean(ACCESS_TOKEN1), pyocean.DigitalOcean(ACCESS_TOKEN2)]
+digitalocean = accountsList[0] #init digitalocean
+
+max_snaps = 15
+femail = "EMAIL"
+temail = "EMAIL"
+
+report = ""
 
 def printAllSnapshots():
+    global report
     try:
         for droplet in digitalocean.droplet.all():
-            printSnapshots(droplet)
-            print ""
+            report += printSnapshots(droplet)
     except pyocean.exceptions.DOException as e:
-        print('ERROR: %s' % e)
+        report = ('ERROR: %s\n' % e)
 
 def printSnapshots(droplet = None):
+    global report
     if droplet == None:
         return None
-    print "[Snapshots for:] " + droplet.name
+    report += "[Snapshots for:] " + droplet.name + "\n"
     snaps = droplet.get_snapshots()
     for snap in snaps:
-        print snap
+        report += snap + "\n"
 
 def snapshot(droplet = None):
+    global report
     if droplet == None:
         return None
     try:
-        print "[Power off:] " + droplet.name
+        report += "[Power off:] " + droplet.name + "\n"
         droplet.power_off()
 
-        print "[Create snapshot for:] " + droplet.name
-        droplet.create_snapshot(droplet.name + str(now.day) + str(now.month) + str(now.year))
+        report += "[Create snapshot for:] " + droplet.name + "\n"
+        droplet.create_snapshot(droplet.name + str(datetime.datetime.now().day) + str(datetime.datetime.now().month)\
+                                + str(datetime.datetime.now().year))
 
         printSnapshots(droplet)
-        print ""
     except pyocean.exceptions.DOException as e:
-        print('ERROR: %s' % e)
+        report += ('ERROR: %s' % e)
 
 def snapsToList(snapshots = None):
     if snapshots == None:
@@ -48,27 +58,36 @@ def hasSnap(droplet = None):
     if droplet == None:
         return None
     snaps = snapsToList(droplet.get_snapshots())
-    return snaps[-1].name == (droplet.name + str(now.day) + str(now.month) + str(now.year))
+    return snaps[-1].name == (droplet.name + str(datetime.datetime.now().day) + str(datetime.datetime.now().month)\
+                              + str(datetime.datetime.now().year))
 
 def cleanSnapshots(droplet = None):
+    global report
     if droplet == None:
         return None
     snaps = snapsToList(droplet.get_snapshots())
     if len(snaps) <= max_snaps:
-        print "[No need to clean snaps for:] " + droplet.name
+        report += "[No need to clean snaps for:] " + droplet.name + "\n"
     else:
-        while len(snaps = snapsToList(droplet.get_snapshots())) > 5:
-            print "[TASK: Destroying snap:] " + snaps.name
+        while len(snaps) > max_snaps:
+            report += "[TASK: Destroying snap:] " + snaps[0].name + "\n"
             snaps[0].destroy()
-            print "[DONE: left:] " + str(len(snaps)-6)
+            report += "[DONE: left:] " + str(len(snaps)-(max_snaps+1)) + "\n"
+            snaps = snapsToList(droplet.get_snapshots())
+    return report
 
 if __name__=="__main__":
     try:
-        for droplet in digitalocean.droplet.all():
-            if hasSnap(droplet) == False:
-                snapshot(droplet)
-            else:
-                print "[Snapshot already taken for:] " + droplet.name
-            cleanSnapshots(droplet)
+        for accout in accountsList:
+            digitalocean = accout
+            for droplet in digitalocean.droplet.all():
+                if hasSnap(droplet) == False:
+                    snapshot(droplet)
+                else:
+                    report += "[Snapshot already taken for:] " + droplet.name + "\n"
+                if not droplet_status.droplet_down(droplet):
+                    alert.send_alert_droplet_down(femail, temail, droplet)
+                cleanSnapshots(droplet)
     except pyocean.exceptions.DOException as e:
-        print('ERROR: %s' % e)
+        report += ('ERROR: %s' % e)
+    alert.send_report(femail, temail, report)
