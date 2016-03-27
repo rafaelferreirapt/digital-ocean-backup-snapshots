f843df7ea9a9939a11edcc06977377d087c063e5
Rafael Ferreira <mail@rafaelferreira.pt>
2016-03-27 02:13:14 +0100

    Fix important issues, now it works very well!

--- alert.py
+++ alert.py
@@ -1,43 +1,42 @@
-__author__ = 'gipmon'
-from email.mime.text import MIMEText
-import smtplib
 import datetime
 import json
+import smtplib
+from email.mime.text import MIMEText
 
 
-def send_alert_droplet_down(femail, temail, droplet):
-    send_email(femail, temail, "[DIGITAL OCEAN BACKUPS] Port down", "The droplet: " + droplet.name + " is down!")
+class Alert:
+    def __init__(self, from_email, to_email):
+        config_file = open("config.json", "r")
+        backup_data = json.load(config_file)
 
+        self.username = backup_data["username"]
+        self.password = backup_data["password"]
+        self.smtp_url = backup_data["smtp_url"]
+        self.smtp_port = backup_data["smtp_port"]
 
-def send_report(femail, temail, report):
-    send_email(femail, temail, "[DIGITAL OCEAN BACKUPS][REPORT][" + str(datetime.datetime.now().day) + "/" + \
-               str(datetime.datetime.now().month) + "/" + str(datetime.datetime.now().year) + "]", report)
+        self.from_email = from_email
+        self.to_email = to_email
 
+    def send_alert_droplet_down(self, droplet):
+        self.send_email("[DIGITAL OCEAN BACKUPS] Port down", "The droplet: " + droplet.name + " is down!")
 
-def send_email(femail, temail, subject, text):
-    config_file = open("config.json", "r")
-    backup_data = json.load(config_file)
+    def send_report(self, report):
+        self.send_email("[DIGITAL OCEAN BACKUPS][REPORT][" + str(datetime.datetime.now().day) + "/" + str(
+            datetime.datetime.now().month) + "/" + str(datetime.datetime.now().year) + "]", report)
 
-    username = backup_data["username"]
-    password = backup_data["password"]
-    smtp_url = backup_data["smtp_url"]
-    smtp_port = backup_data["smtp_port"]
+    def send_email(self, subject, text):
+        msg = MIMEText(text)
+        msg['From'] = self.from_email
+        msg['To'] = self.to_email
+        msg['Subject'] = subject
 
-    fromaddr = femail
-    toaddr = temail
+        server = smtplib.SMTP(self.smtp_url, self.smtp_port)
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+        server.login(self.username, self.password)
 
-    msg = MIMEText(text)
-    msg['From'] = fromaddr
-    msg['To'] = toaddr
-    msg['Subject'] = subject
+        text = msg.as_string()
 
-    server = smtplib.SMTP(smtp_url, smtp_port)
-    server.ehlo()
-    server.starttls()
-    server.ehlo()
-    server.login(username, password)
-
-    text = msg.as_string()
-
-    server.sendmail(fromaddr, toaddr, text)
-    server.quit()
+        server.sendmail(self.from_email, self.to_email, text)
+        server.quit()
--- snapshots.py
+++ snapshots.py
@@ -1,9 +1,9 @@
 import pyocean
 import datetime
-import alert
-import droplet_status
 import time
+import socket
 import json
+from alert import Alert
 
 
 class Backup:
@@ -12,67 +12,70 @@ class Backup:
         self.report = ""
         config_file = open("config.json", "r")
         self.backup_data = json.load(config_file)
-
-    def print_all_snapshots(self):
-        try:
-            for droplet in self.digital_ocean.droplet.all():
-                self.report += self.print_snapshots(droplet)
-        except pyocean.exceptions.DOException as e:
-            self.report = ('ERROR: %s\n' % e)
+        self.alert = Alert(self.backup_data["femail"], self.backup_data["temail"])
 
     def print_snapshots(self, droplet=None):
         if droplet is None:
             return None
 
         self.report += "[Snapshots for:] " + droplet.name + "\n"
-        snaps = droplet.get_snapshots()
 
-        for snap in snaps:
+        for snap in droplet.get_snapshots():
             self.report += str(snap) + "\n"
 
     def snapshot(self, droplet=None):
         if droplet is None:
             return None
         try:
-            self.report += "[Power off:] " + droplet.name + "\n"
+            # if the droplet is active we must power of the droplet and then
+            # make the snapshot and then power on again
+            if droplet.status == "active":
+                try:
+                    self.report += "[Power off:] " + droplet.name + "\n"
 
-            try:
-                droplet.power_off()
-                time.sleep(60)
-            except pyocean.exceptions.DOException as e:
-                if e.code is not 422:
-                    self.report += ('ERROR: %s' % e)
+                    # now power off
+                    droplet.power_off()
+                    time.sleep(20)
 
-            time.sleep(60)
+                    # now we must refresh the droplet state
+                    droplet = backup.digital_ocean.droplet.get(droplet.id)
+                except pyocean.exceptions.DOException as e:
+                    if e.code is not 422:
+                        self.report += ('ERROR: %s' % e)
 
             self.report += "[Create snapshot for:] " + droplet.name + "\n"
             droplet.create_snapshot(droplet.name + str(datetime.datetime.now().day) +
                                     str(datetime.datetime.now().month) + str(datetime.datetime.now().year))
 
+            # now we must refresh the droplet state
+            droplet = backup.digital_ocean.droplet.get(droplet.id)
+
+            # print all the snapshots taken so far
             self.print_snapshots(droplet)
+            time.sleep(20)
         except pyocean.exceptions.DOException as e:
             self.report += ('ERROR: %s' % e)
 
     @staticmethod
-    def snaps_to_list(snap=None):
-        if snap is None:
-            return None
-        return [snap for snap in snap]
-
-    @staticmethod
-    def has_snap(droplet=None):
+    def need_to_backup(droplet=None):
         if droplet is None:
-            return None
+            return False
 
-        snaps = Backup.snaps_to_list(droplet.get_snapshots())
+        # get a list of snapshots
+        snaps = [snap for snap in droplet.get_snapshots()]
 
-        return snaps[-1].name == (droplet.name + str(datetime.datetime.now().day) + str(datetime.datetime.now().month) +
+        # if the last one snapshot has a today date, it don't need to be backup
+        if len(snaps) == 0:
+            return True
+
+        return snaps[-1].name != (droplet.name + str(datetime.datetime.now().day) + str(datetime.datetime.now().month) +
                                   str(datetime.datetime.now().year))
 
     def clean_snapshots(self, droplet=None):
         if droplet is None:
             return None
-        snaps = self.snaps_to_list(droplet.get_snapshots())
+
+        snaps = [snap for snap in droplet.get_snapshots()]
 
         if len(snaps) <= self.backup_data["max_snaps"]:
             self.report += "[No need to clean snaps for:] " + droplet.name + "\n"
@@ -81,7 +84,8 @@ class Backup:
                 self.report += "[TASK: Destroying snap:] " + snaps[0].name + "\n"
                 snaps[0].destroy()
                 self.report += "[DONE: left:] " + str(len(snaps) - (self.backup_data["max_snaps"] + 1)) + "\n"
-                snaps = self.snaps_to_list(droplet.get_snapshots())
+                # refresh the list
+                snaps = [snap for snap in droplet.get_snapshots()]
 
         return self.report
 
@@ -89,37 +93,37 @@ class Backup:
         try:
             for account in self.backup_data["accountsList"]:
                 self.digital_ocean = pyocean.DigitalOcean(account)
-                
-                for droplet in backup.digital_ocean.droplet.all():
-                    if droplet.status == "off":
-                        try:
-                            droplet.power_on()
-                            time.sleep(60)
-                        except pyocean.exceptions.DOException as e:
-                            if e.code is 422:
-                                continue
-                            else:
-                                self.report += ('ERROR: %s' % e)
 
-                    if self.has_snap(droplet) is False:
+                for droplet in backup.digital_ocean.droplet.all():
+                    # refresh the state of the digital ocean droplet
+                    droplet = backup.digital_ocean.droplet.get(droplet.id)
+
+                    # if need to be backup the droplet must be made a snapshot to the droplet
+                    if self.need_to_backup(droplet):
                         backup.snapshot(droplet)
-                        time.sleep(60)
                     else:
                         self.report += "[Snapshot already taken for:] " + droplet.name + "\n"
 
                     # see if apache is running
-                    if not droplet_status.droplet_down(droplet):
-                        alert.send_alert_droplet_down(self.backup_data["femail"], self.backup_data["temail"], droplet)
+                    host = droplet.networks['v4'][0]['ip_address']
+                    port = 80
+
+                    try:
+                        socket.socket().connect((host, port))
+                    except socket.error:
+                        self.alert.send_alert_droplet_down(droplet)
 
                     self.clean_snapshots(droplet)
                     self.report += "\n"
 
-                # make all droplets active
+                # make all droplets active, they must be active
                 for droplet in backup.digital_ocean.droplet.all():
+                    # refresh the state of the digital ocean droplet
+                    droplet = backup.digital_ocean.droplet.get(droplet.id)
+
                     if droplet.status == "off":
                         try:
                             droplet.power_on()
-                            time.sleep(60)
                         except pyocean.exceptions.DOException as e:
                             if e.code is 422:
                                 continue
@@ -128,7 +132,7 @@ class Backup:
 
         except pyocean.exceptions.DOException as e:
             self.report += ('ERROR: %s' % e)
-        alert.send_report(self.backup_data["femail"], self.backup_data["temail"], self.report)
+        self.alert.send_report(self.report)
 
 
 if __name__ == "__main__":
